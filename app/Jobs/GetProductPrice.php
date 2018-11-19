6163a3602f25f0dcff3b08dfe4fc72db718b417f
Herbert Balagtas <hbalagtas@gmail.com>
2018-11-18 19:42:58 -0500

    Database queues now working

--- app/Jobs/GetProductPrice.php
+++ app/Jobs/GetProductPrice.php
@@ -8,6 +8,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use PriceTracker\Product;
+use PriceTracker\Price;
 use PriceTracker\SiteConfig;
 
 class GetProductPrice implements ShouldQueue
